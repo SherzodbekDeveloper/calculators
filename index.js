36df005ba8467bed8c5b18f7e939b9d45d356337
SherzodbekDeveloper <omilxanovs7@gmail.com>
2024-06-24 22:49:47 +0500

    as
    as

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+
+let bir = document.getElementById('bir')
+let operator = document.getElementById('operator');
+let ikki = document.getElementById('ikki')
+let btn = document.getElementById('btn');
+let p  = document.getElementById('data')
+let result;
+
+btn.onclick = salom;
+
+function salom(){
+    const obj = {
+        first: bir.value,
+        second: ikki.value,  
+        operator: operator.value ,
+        
+    }
+    if ( obj.operator == "+"){
+        result  =  +obj.first + +obj.second;
+    }
+    else if( obj.operator == "-"){
+        result  =  obj.first - obj.second
+        }
+    else if( obj.operator == "*"){
+        result  = obj.first * obj.second
+    }
+    else if (obj.operator == "/"){
+        result  = obj.first / obj.second
+    }
+
+    
+    p.innerHTML = "<h1 style='color:white; text-transform:uppercase;'>" + result +"</h1>"
+}
